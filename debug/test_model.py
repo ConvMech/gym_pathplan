aa812ae6a9d4e0715b880e8c4a9c3ecaac90f750
qianqianhu577 <qqh0402@hotmail.com>
2018-12-04 12:09:12 +0800

    some changes

--- debug/test_model.py
+++ debug/test_model.py
@@ -5,17 +5,17 @@ from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv
 
 # Create and wrap the environment
-env = gym.make('PathAngle-v0')
+env = gym.make('PathCNN-v0')
 env = DummyVecEnv([lambda: env])
 
 # Load the trained agent
-model = PPO2.load("PathAngle")
+model = PPO2.load("PathAngle_cnn1")
 #model = A2C.load("ppo2_lunar")
 # Enjoy trained agent
 
 obs = env.reset()
 count = 0
-for _ in range(100):
+for i in range(100):
 	env.reset()
 	done = False
 	while not done:
@@ -25,5 +25,6 @@ for _ in range(100):
 		#time.sleep(0.1)
 		if r == 100:
 			count += 1
+	#print('success rate:',count/i)
 
 print('success rate:',count/100.0)
